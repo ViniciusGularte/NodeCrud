9994a9fc9e4424f1fd78b1b19d356e6ca8ca93fd
vinicius martin <viniciusgulartemartin@gmail.com>
2018-10-17 09:21:14 -0300

    new struture

--- src/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql')
-const pool  = mysql.createPool({
-  connectionLimit : 10,
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'teste'
-});
-exports.pool = pool;
