935309f5c245a78a175d9a596ba8561ef9b5e76d
Vinicius G. Martin <viniciusgulartemartin@gmail.com>
2018-10-19 20:37:47 -0300

    update controller

---
+++ pessoas/pessoas.controller.js
@@ -0,0 +1,75 @@
+var Pessoas = require('./pessoas.dao');
+
+exports.createPessoas = function (req, res, next) {
+    var pessoa = {
+        name: req.body.name,
+        description: req.body.description
+    };
+
+    Pessoas.create(pessoas, function(err, pessoas) {
+        if(err) {
+            res.json({
+                error : err
+            })
+        }
+        res.json({
+            message : "Pessoa criada com sucesso"
+        })
+    })
+}
+
+exports.getHeros = function(req, res, next) {
+    Pessoas.get({}, function(err, pessoas) {
+        if(err) {
+            res.json({
+                error: err
+            })
+        }
+        res.json({
+          pessoas: pessoas
+        })
+    })
+}
+
+exports.getPessoa = function(req, res, next) {
+    Pessoas.get({name: req.params.name}, function(err, pessoas) {
+        if(err) {
+            res.json({
+                error: err
+            })
+        }
+        res.json({
+            pessoas: pessoas
+        })
+    })
+}
+
+exports.updatePessoa = function(req, res, next) {
+    var pessoa = {
+        name: req.body.name,
+        description: req.body.description
+    }
+    Pessoas.update({_id: req.params.id}, pessoa, function(err, pessoa) {
+        if(err) {
+            res.json({
+                error : err
+            })
+        }
+        res.json({
+            message : "Pessoa atualizada com sucesso"
+        })
+    })
+}
+
+exports.removePessoa = function(req, res, next) {
+    Heros.delete({_id: req.params.id}, function(err, pessoa) {
+        if(err) {
+            res.json({
+                error : err
+            })
+        }
+        res.json({
+            message : "Pessoa deletada com sucesso"
+        })
+    })
+}
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var properties = require('./config/properties');
 var db = require('./config/database');
 var app = express();
 
-// call the database connectivity function
 db();
 
 app.listen(properties.PORT, (req, res) => {
